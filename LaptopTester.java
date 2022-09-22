bd3d27de3967c24f7c8c1b853469bf38bf518e7b
kavya-gayakawad <kavyagayakawad.xworkz@gmail.com>
2022-09-22 21:03:06 +0530

    committing methods related files

---
+++ LaptopTester.java
@@ -0,0 +1,41 @@
+class LaptopTester{
+
+public static void main(String a[]){
+System.out.println("main method is started");
+
+
+
+
+Speaker.brand = "Lenovo";
+Speaker.color = "Black";
+
+Speaker.onOrOff();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.increaseVolume();
+Speaker.onOrOff();
+Speaker.increaseVolume();
+	
+Speaker.onOrOff();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+Speaker.decreaseVolume();
+Speaker.onOrOff();
+  Speaker.decreaseVolume();
+
+System.out.println("main method is ended");
+
+
+
+
+}
+
+
+
+
+
+}
---
+++ Mobile.java
@@ -0,0 +1,73 @@
+class Mobile{
+
+static String brand;
+static String color;
+static double price;
+static int minVolume;
+static int maxVolume = 8;
+static int currentVolume;
+static boolean isConnected;
+
+public static boolean onOrOff(){
+
+System.out.println("inside onOrOff()");
+  if (isConnected == false){
+      isConnected = true;
+System.out.println("Mobile is turned on");
+ 
+ }
+
+
+ else if(isConnected == true){
+    isConnected = false;
+System.out.println("Mobile is turned off");
+ 
+ }
+System.out.println("end of onOrOff()");
+ return isConnected;
+
+}
+public static void increaseVolume(){
+	
+System.out.println("inside increaseVolume()");
+// false == true
+if(isConnected == true){
+if(currentVolume < maxVolume){
+     currentVolume = currentVolume + 1 ;
+	 System.out.println("The currentVolume is :" + currentVolume);
+}
+else {
+	System.out.println("Max Volume reached...");
+}
+
+System.out.println("end of increaseVolume");
+
+}
+	else{
+	System.out.println("Gube.. First turn on the Mobile");
+	
+}
+}
+
+   public static void decreaseVolume(){
+   System.out.println("inside decreaseVolume()");
+ 
+ if(isConnected == true){
+ if(currentVolume > minVolume){
+	 currentVolume = currentVolume - 1 ;
+	 System.out.println("The currentVolume is :" + currentVolume);
+ }	 
+ else{
+	 System.out.println("min Volume reached...");
+ }
+ System.out.println("end of decreaseVolume");
+ }
+
+else{
+	System.out.println("Gube First turn on the Mobile");
+}
+
+	 
+}
+
+}
