35cbe855dc3732e58edd46ba487a7d278cd6a920
kavya-gayakawad <kavyagayakawad.xworkz@gmail.com>
2022-09-22 11:33:40 +0530

    committing methodoverloading files

---
+++ Addition.java
@@ -0,0 +1,16 @@
+class Addition{
+public static void add(int a, int b)
+{
+System.out.println(a+b);
+
+}
+
+public static void add(int a, int b, int c)
+{
+System.out.println(a+b+c);
+}
+
+}
+
+
+
---
+++ AdditionTester.java
@@ -0,0 +1,13 @@
+class AdditionTester{
+
+public static void main(String args[]){
+
+
+Addition.add(23,65);
+Addition.add(245,756,654);
+
+
+}
+
+
+}
