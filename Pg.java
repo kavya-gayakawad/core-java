2b1bd3527963faf77fe3020edb9e09e44aa6372e
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-10-05 19:42:50 +0530

    Add files via upload

---
+++ Pg.java
@@ -0,0 +1,40 @@
+class Pg{
+
+static String pgName;
+static String pgAddress;
+static long contactNo;
+
+static void setPgName(String pName){
+  pgName = pName;
+
+}
+static String getPgName(){
+return pgName;
+
+}
+static void setPgAddress(String pAddress){
+ pgAddress = pAddress;
+}
+static String getPgAddress(){
+return pgAddress;
+}
+static void setContactNo(long cNo){
+ contactNo = cNo;
+
+}
+static long getContactNo(){
+  return contactNo;
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
---
+++ PgTester.java
@@ -0,0 +1,20 @@
+class PgTester{
+
+public static void main(String args[]){
+
+Pg.setPgName("Golden Pg");
+Pg.setPgAddress("Rajajinagar");
+Pg.setContactNo(7022228942L);
+
+System.out.println(Pg.getPgName());
+System.out.println(Pg.getPgAddress());
+System.out.println(Pg.getContactNo());
+
+
+
+}
+
+
+
+
+}
