f9e2a381442868a7fa800c39c3a63d6e14a638e8
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-17 12:05:25 +0530

    Add files via upload

---
+++ CsEngineer.java
@@ -0,0 +1,11 @@
+package com.xworkz.engineer.cs;
+
+import com.xworkz.engineer.Engineer;
+
+public class CsEngineer extends Engineer {
+	public void solveProblems() {
+		
+    System.out.println("Sovling Problems by CsEngineer");
+	}
+
+}
