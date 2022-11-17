1c9358a17015093d8a66a36ec6f539b897d4afbc
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-17 12:09:45 +0530

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,10 @@
+package com.xworkz.fruit.fruitapp;
+
+public class Apple extends Fruit {
+	
+	public void getNutrition() {
+		System.out.println("Apple Fruits are great source of vitamins");
+	}
+
+
+}
