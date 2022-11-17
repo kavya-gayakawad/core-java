d7f8edf0aa5f847eea83b2a0cb375461803a20e7
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-17 12:11:06 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,9 @@
+package com.xworkz.fruit.fruitapp;
+
+public class Mango extends Fruit {
+
+	public void getNutrition() {
+		System.out.println("Mango Fruits are great source of vitamins");
+	}
+
+}
