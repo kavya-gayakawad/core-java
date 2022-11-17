34ebf9efb34f65c3948c0c8d2020a68e752e746b
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-17 12:06:38 +0530

    Add files via upload

---
+++ EcEngineer.java
@@ -0,0 +1,17 @@
+package com.xworkz.engineer.engineerapp.ec;
+
+import com.xworkz.engineer.Engineer;
+
+public class EcEngineer extends Engineer {
+	@Override
+	public void solveProblems(){
+		System.out.println("Solving problems by EcEngineer");
+			
+		//public int solveProblems(long a,int){
+		//System.out.println("Solving problems by EcEngineer");
+			
+		
+
+
+}
+}
