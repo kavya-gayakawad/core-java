5272e02057e42810d5b6caca180ed64156f418fc
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-29 20:21:05 +0530

    Add files via upload

---
+++ Cooler.java
@@ -0,0 +1,39 @@
+class Cooler{
+static String brandName;
+static String color;
+static int price;
+public static void main(String a[]){
+
+Cooler.brandName = "Bajaj";
+Cooler.color = "White";
+Cooler.price = 10000;
+
+System.out.println("brandName is " + brandName);
+System.out.println("color is " + color);
+System.out.println("price is " + price);
+
+Cooler.brandName = "Usha";
+Cooler.color = "White";
+Cooler.price = 10000;
+
+System.out.println("brandName is " + brandName);
+System.out.println("color is " + color);
+System.out.println("price is " + price);
+
+Cooler.brandName = "Blue star";
+Cooler.color = "White";
+Cooler.price = 10000;
+
+System.out.println("brandName is " + brandName);
+System.out.println("color is " + color);
+System.out.println("price is " + price);
+
+
+
+
+
+}
+
+
+
+}
