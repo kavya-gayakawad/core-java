eeeb8af5f22e085ca18b26140c6b2e1329223cc3
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-29 20:23:23 +0530

    Add files via upload

---
+++ Market.java
@@ -0,0 +1,19 @@
+class Market{
+static String vegatables[];
+static String marketName = "kasat Bazar";
+public static void main(String a[]){
+String vegatables[] = {"carrot", "cabbage", "potato", "beans", "cucumber", "onion", "domestic", "brinjal"};
+System.out.println("Welcome to " + marketName);
+System.out.println("List of Vegatables");
+				   
+
+System.out.println("vegatables section");	   
+		   for(int z = 0; z < vegatables.length; z++){
+			 System.out.println(vegatables[z] +  "  "); 
+			   
+
+}
+
+
+}        
+}                                               
