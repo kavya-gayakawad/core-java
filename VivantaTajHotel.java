9e3d4525acafb815afa646cc45281ad4ebebd098
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-29 20:37:32 +0530

    Add files via upload

---
+++ VivantaTajHotel.java
@@ -0,0 +1,34 @@
+class VivantaTajHotel
+
+{
+public static void main(String a[])
+{
+String type = "5 Star Hotel";	
+String address = "Mg Road";
+String[] managerNames = {"Akshata", "Lakshmi", "Sangeeta", "Kavya", "Manjula"};
+int noOfManagers = 5;
+String foodMenu[] = {"Sezwaan fried rice", "Gobhi manchoori", "Noodles", "Pizza", "Burger", "Kaaju kurma", "Egg hundi", "Butternon", "Tandoori roti", "Ice Cream" };
+System.out.println("Welcome to VivavantaTajHotel");
+System.out.println("Destination : "+ address 
+);
+System.out.println("Name of List of Managers");
+System.out.println("Food menu");
+
+
+   for(int c = 0; c < managerNames.length; c++){
+System.out.println(managerNames[c]);	
+	
+}
+   for(int v = 0; v < foodMenu.length; v++){
+	   System.out.println(foodMenu[v]);
+   }
+
+
+
+}
+
+
+
+
+
+}
