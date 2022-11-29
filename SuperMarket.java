3ad847c49c4530037358f326c97a91b827d45d06
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-11-29 20:34:28 +0530

    Add files via upload

---
+++ SuperMarket.java
@@ -0,0 +1,106 @@
+class SuperMarket{
+static String vegatables[];
+static String fruits[];
+static String biscuits[];
+static String perfumes[];
+static String clothes[];
+static String cosmatics[];
+static String electronicDevices[];
+static String superMarketName = "Reliance Mart";
+//static String vegatables[]; 
+
+ 
+
+public static void main(String a[]){
+String vegatables[] = {"Carrot", "tomato", "potato", "onion", "brinjol", "domestic", "cucumber", "cabbage"};
+String fruits[] = {"apple", "orange", "mango", "pineapple", "chikku", "banana", "grapes", "kiwi"};
+String biscuits[] = {"mariegold", "parleg", "crack jack", "chaska maska", "oreo", "happy happy", "horlics", "cream biscuit"};
+String perfumes[] = {"eva", "yardley", "fogg", "engage", "envy", "nivea", "spinz", "axe"};
+String clothes[] = {"shirt", "pant", "tops", "jeans", "kurta", "lehenga", "sarees", "skirts"};
+String cosmatics[] = {"lipstick", "perfumes", "fingernail polish", "hair colors", "permanent waves", "moisturizers", "deodrants", "eyelashes"};
+Strings electronicDevices[] = {"clock", "tv", "radio", "computer", "laptop", "camera", "projector", "speaker"};
+System.out.println("Welcome to "+ superMarketName);
+System.out.println("List of Vegatables");
+     System.out.println(
+	       vegatables[0] + "\n" + 
+		   vegatables[1] + "\n" +
+		   vegatables[2] + "\n" + 
+		   vegatables[3] + "\n" + 
+		   vegatables[4] + "\n" + 
+		   vegatables[5] + "\n" + 
+		   vegatables[6] + "\n" + 
+		   vegatables[7]);
+System.out.println("*************");
+System.out.println("List of Fruits");
+System.out.println(
+                   fruits[0] + "\n" +
+				   fruits[1] + "\n" +
+				   fruits[2] + "\n" +
+				   fruits[3] + "\n" +
+				   fruits[4] + "\n" +
+				   fruits[5] + "\n" +
+				   fruits[6] + "\n" +
+				   fruits[7]);
+System.out.println("*************");
+System.out.println("List of Biscuits");
+System.out.println(
+                   biscuits[0] + "\n" +
+				   biscuits[1] + "\n" +
+				   biscuits[2] + "\n" +
+				   biscuits[3] + "\n" +
+				   biscuits[4] + "\n" +
+				   biscuits[5] + "\n" +
+				   biscuits[6] + "\n" +
+				   biscuits[7]);
+				   
+System.out.println("*************");
+System.out.println("List of Perfume");
+System.out.println(
+                   perfumes[0] + "\n" +
+				   perfumes[1] + "\n" +
+				   perfumes[2] + "\n" +
+				   perfumes[3] + "\n" +
+				   perfumes[4] + "\n" +
+				   perfumes[5] + "\n" +
+				   perfumes[6] + "\n" +
+				   perfumes[7]);
+System.out.println("*************");
+System.out.println("List of clothes");
+System.out.println(
+                   clothes[0] + "\n" +
+				   clothes[1] + "\n" +
+				   clothes[2] + "\n" +
+				   clothes[3] + "\n" +
+				   clothes[4] + "\n" +
+				   clothes[5] + "\n" +
+				   clothes[6] + "\n" +
+				   clothes[7]);
+				   System.out.println("*************");
+System.out.println("List of cosmatics");
+System.out.println(
+                   cosmatics[0] + "\n" +
+				   cosmatics[1] + "\n" +
+				   cosmatics[2] + "\n" +
+				   cosmatics[3] + "\n" +
+				   cosmatics[4] + "\n" +
+				   cosmatics[5] + "\n" +
+				   cosmatics[6] + "\n" +
+				   cosmatics[7]);
+System.out.println("*************");
+System.out.println("List of electronicDevices");
+System.out.println(
+                   electronicDevices[0] + "\n" +
+				   electronicDevices[1] + "\n" +
+				   electronicDevices[2] + "\n" +
+				   electronicDevices[3] + "\n" +
+				   electronicDevices[4] + "\n" +
+				   electronicDevices[5] + "\n" +
+				   electronicDevices[6] + "\n" +
+				   electronicDevices[7]);
+
+
+			   
+}
+
+}
+
