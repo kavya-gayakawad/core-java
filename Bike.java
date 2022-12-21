a408b794c6ef1629ea312bc8f2162d31bb5045f3
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-12-21 21:17:14 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,37 @@
+package com.xworkz.bike;
+
+import java.util.ArrayList;
+
+public class Bike {
+	public static void main(String args[]) {
+		
+		ArrayList<String> bike=new ArrayList<String>();
+		
+		bike.add("Royal Enfield");
+		bike.add("Hero Splendor");
+		bike.add("HF Deluxe");
+		bike.add("TVS sport");
+		bike.add("TVS Apache RR 310");
+		bike.add("Bajaj CT 100");
+		bike.add("Honda Shine");
+		bike.add("Yamaha YZF-R15");
+		bike.add("Hero Glamour");
+		bike.add("Bajaj Pulsar 150");
+		bike.add("TVS jupiter");
+		bike.add("Honda SP 125");
+		bike.add("KTM 390 Duke");
+		bike.add("Tvs Rider 125");
+		bike.add("Yamaha Rx 100");
+		bike.add("Avenger 200 DTS-i");
+		bike.add("Discover 100 M");
+		bike.add("Royal Enfield Bullet 350");
+		
+		System.out.println(bike);
+		
+		bike.clear();
+		
+		System.out.println(bike.size());
+	
+	}
+
+}
