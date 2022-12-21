6156fb3ffdc847b6cb206a427dea5a25285c9d4e
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-12-21 21:17:50 +0530

    Add files via upload

---
+++ Coupon.java
@@ -0,0 +1,29 @@
+package com.xworkz.coupons;
+
+import java.util.ArrayList;
+
+public class Coupon {
+	
+	public static void main(String args[]) {
+		
+		ArrayList<String> coupon = new ArrayList<String>();
+		
+		coupon.add("Percent of coupons");
+		coupon.add("Free shipping coupon");
+		coupon.add("Free gift with purchase ");
+		coupon.add("BOGO coupons");
+		coupon.add("Automatic coupons");
+		coupon.add("Gift card with purchase");
+		coupon.add("Mobile coupons");
+		coupon.add("Free Trials coupons");
+		coupon.add("Buy 1 Get 1 Free coupons");
+		coupon.add("Abandoned Cart coupons");
+		coupon.add("First-Time Purchase Discount coupons");
+		coupon.add("One-click coupons");
+		
+		System.out.println(coupon);
+		System.out.println(coupon.size());
+		
+	}
+
+}
