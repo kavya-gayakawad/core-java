9ecf570ef7a98db8e8709b3a200345fbf4f5c356
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-12-21 21:19:10 +0530

    Add files via upload

---
+++ Mall.java
@@ -0,0 +1,69 @@
+package com.xworkz.malls;
+
+import java.util.ArrayList;
+
+public class Mall {
+	
+	public static void main(String args[]) {
+		
+		ArrayList<String> mall=new ArrayList<String>();
+		
+		mall.add("GT");
+		mall.add("Orion");
+		mall.add("Lulu");
+		mall.add("Nexus");
+		mall.add("Mantri");
+		mall.add("UB");
+		mall.add("Garuda");
+		mall.add("Phoenix");
+		mall.add("Cosmos");
+		mall.add("Gopalan");
+		mall.add("Innovative Multiplex");
+		mall.add("Leela Galleria");
+		mall.add("Lido");
+		mall.add("Sigma");
+		mall.add("Swagath Garuda");
+		mall.add("The Collection");
+		mall.add("The Forum");
+		mall.add("Total Mall");
+		mall.add("Royal Meenakshi");
+		mall.add("Phinix");
+		mall.add("Inorbit");
+		mall.add("City center");
+		mall.add("Park Square mall");
+		mall.add("Urban Oasis mall");
+		mall.add("Mall of Mysuru");
+		mall.add("S mall");
+		mall.add("Bharat mall");
+		mall.add("Bangalore Central");
+		mall.add("City center mall");
+		mall.add("Mall De Goa");
+		mall.add("Lulu mall Lucknow");
+		mall.add("DB city mall");
+		mall.add("Hilite mall");
+		mall.add("R city mall");
+		mall.add("Elante mall");
+		mall.add("Mantri Square");
+		mall.add("VR Chennai");
+		mall.add("Esplanade");
+		mall.add("Z Square mall");
+		mall.add("Vegas mall");
+		
+		System.out.println(mall);
+		
+		mall.clear();
+		
+		System.out.println(mall.size());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
