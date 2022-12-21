c8c38a056c4b3f05df79f1b038b666c5bcbba0f4
kavya-gayakawad <113685837+kavya-gayakawad@users.noreply.github.com>
2022-12-21 21:18:25 +0530

    Add files via upload

---
+++ Currencies.java
@@ -0,0 +1,44 @@
+package com.xworkz.currencies;
+
+import java.util.ArrayList;
+
+public class Currencies {
+	
+	public static void main(String args[]) {
+		
+		
+		ArrayList<String> currency=new ArrayList<String>();
+		
+		currency.add("United states Doller");
+		currency.add("Euro");
+		currency.add("Pound sterling");
+		currency.add("Indian Rupee");
+		currency.add("Austalian Doller");
+		currency.add("Indian Rupee");
+		currency.add("Canadian Doller");
+		currency.add("Albanian Lek");
+		currency.add("East Caribben Doller");
+		currency.add("Japanese Yen");
+		currency.add("Singapore Doller");
+		currency.add("Swiss Franc");
+		currency.add("Russian Ruble");
+		currency.add("Danish Krone");
+		currency.add("South African Rand");
+		currency.add("Philippine peso");
+		currency.add("Mexican Peso");
+		currency.add("Chinese Yuan");
+		currency.add("Turkish lira");
+		currency.add("Israeli Shekel");
+		currency.add("New Zealand Doller");
+		currency.add("Brazilian Real");
+		currency.add("Romanian Leu");
+		currency.add("Malaysian Ringgit");
+		currency.add("Combodian Riel");
+		currency.add("Hong Kong Doller");
+		currency.add("Colombian Peso");
+		
+	System.out.println(currency);
+	System.out.println(currency.size());
+	}
+
+}
