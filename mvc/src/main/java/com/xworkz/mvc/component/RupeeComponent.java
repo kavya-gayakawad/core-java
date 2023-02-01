331574edfc1ac53036e42e09f7ed1936c95cb3ba
kavya8217098558 <kavyagayakawad.xworkz@gmail.com>
2023-02-01 09:48:56 +0530

    committing mvc files

---
+++ mvc/src/main/java/com/xworkz/mvc/component/RupeeComponent.java
@@ -0,0 +1,12 @@
+package com.xworkz.mvc.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RupeeComponent {
+	
+	public RupeeComponent() {
+		System.out.println("created "+this.getClass().getSimpleName());
+	}
+
+}
