b522eb6bf199bf1eae8565d871751bfed5a3bf83
kavya8217098558 <kavyagayakawad.xworkz@gmail.com>
2023-02-01 17:59:24 +0530

    committing mvcfiles

---
+++ fooditem/src/main/java/com/xworkz/fooditem/configuration/FoodItemConfiguration.java
@@ -0,0 +1,13 @@
+package com.xworkz.fooditem.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xworkz.fooditem")
+public class FoodItemConfiguration {
+
+	public FoodItemConfiguration() {
+	System.out.println("created "+this.getClass().getSimpleName());
+	}
+}
